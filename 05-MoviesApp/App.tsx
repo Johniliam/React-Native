f12d96dffbfa4c05e8ef3fe18160dd400108f37e
Johnatan Ramirez <johniliam2151@gmail.com>
2021-09-04 18:36:59 -0500

    Gradient & Fade (rest)

--- 05-MoviesApp/App.tsx
+++ 05-MoviesApp/App.tsx
@@ -4,11 +4,28 @@ import React from 'react'
 import { Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { Navigation } from './src/navigation/Navigation';
+import { FadeScreen } from './src/screens/FadeScreen';
+import { GradientProvider } from './src/context/GradientContext';
+
+interface Props {
+  children: JSX.Element | JSX.Element[]
+}
+
+const AppState = ({ children }: Props) => {
+  return(
+    <GradientProvider>
+      { children }
+    </GradientProvider>
+  )
+}
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Navigation/>  
+      <AppState>
+        <Navigation/>  
+      </AppState>
+      {/* <FadeScreen/> */}
     </NavigationContainer>
   )
 }
--- 05-MoviesApp/src/screens/HomeScreen.tsx
+++ 05-MoviesApp/src/screens/HomeScreen.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import { ActivityIndicator, Dimensions, View, ScrollView } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+import ImageColors from 'react-native-image-colors';
 
 import Carousel from 'react-native-snap-carousel';
 
@@ -8,6 +10,9 @@ import { useMovies } from '../hooks/useMovies';
 import { styles } from '../theme/styles';
 import { MoviePoster } from '../components/MoviePoster';
 import { HorizontalSlider } from '../components/HorizontalSlider';
+import { GradientBackground } from '../components/GradientBackground';
+import { getImageColors } from '../helpers/getColores';
+import { GradientContext } from '../context/GradientContext';
 
 const { width: windowWidth } = Dimensions.get('window');
 
@@ -15,7 +20,22 @@ export const HomeScreen = () => {
 
     const { nowPlaying, popular, topRated, upComing, isLoading } = useMovies();
     const { top } = useSafeAreaInsets();
-
+    const { setMainColors } = useContext( GradientContext ) 
+    
+    const getPosterColors = async ( index: number ) => {
+        const movie = nowPlaying[index];
+        const uri = "https://image.tmdb.org/t/p/w500" + movie.poster_path
+        
+        const [ primary = 'green', secondary = 'orange' ] = await getImageColors( uri );
+        setMainColors({ primary, secondary })
+    }
+    
+    useEffect(() => {
+        if ( nowPlaying.length > 0 ) {
+            getPosterColors(0)
+        }
+    }, [ nowPlaying ])
+    
     if ( isLoading ) {
         return(
             <View style={styles.loadingIndicator}>
@@ -25,27 +45,30 @@ export const HomeScreen = () => {
     }
 
     return (
-        <ScrollView>
-            <View style={{ marginTop: top + 20 }}>
+        <GradientBackground>
+            <ScrollView>
+                <View style={{ marginTop: top + 20 }}>
 
-                {/* Carrousel Principal */}
-                <View style={{ height: 440 }}>
-                   <Carousel
-                       data= { nowPlaying }
-                       renderItem={ ({ item }: any) => <MoviePoster movie={ item }/> }
-                       sliderWidth= { windowWidth }
-                       itemWidth= { 300 }
-                       inactiveSlideOpacity={1}
-                   />
+                    {/* Carrousel Principal */}
+                    <View style={{ height: 440 }}>
+                    <Carousel
+                        data= { nowPlaying }
+                        renderItem={ ({ item }: any) => <MoviePoster movie={ item }/> }
+                        sliderWidth= { windowWidth }
+                        itemWidth= { 300 }
+                        inactiveSlideOpacity={1}
+                        onSnapToItem={ index => getPosterColors( index ) }
+                    />
+                    </View>
+
+                    {/* Peliculas */}
+                    <HorizontalSlider title={'Populares'} movies={ popular } />
+                    <HorizontalSlider title={'Mejor Calificadas'} movies={ topRated } />
+                    <HorizontalSlider title={'Proximamente'} movies={ upComing } />
+
                 </View>
 
-                {/* Peliculas */}
-                <HorizontalSlider title={'Populares'} movies={ popular } />
-                <HorizontalSlider title={'Mejor Calificadas'} movies={ topRated } />
-                <HorizontalSlider title={'Proximamente'} movies={ upComing } />
-
-            </View>
-
-        </ScrollView>
+            </ScrollView>
+        </GradientBackground>
     )
 }
